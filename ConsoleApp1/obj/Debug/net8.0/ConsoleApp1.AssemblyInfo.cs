855ab6e0c54e6c3f0f43ae15d0b8a1f505e98b3b
escodsm <escodsm@icloud.com>
2024-04-21 12:11:57 -0400

    added console app and winforms

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62bdf8bba9caf1974227d8c5d3f0ceecf6b8cbf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+288792b30f7e7fbf7e9e9410cf798678a679733e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Form1.cs
@@ -0,0 +1,9 @@
+namespace friendly_garbanzo;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
